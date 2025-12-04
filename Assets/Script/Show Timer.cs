0589c5878604c43b51a66f14af61d89c7b1efd89
Ethan Deschamps Pro <146001193+PunkKin@users.noreply.github.com>
2025-12-04 17:58:39 +0100

    Cotcot

---
+++ Assets/Script/Show Timer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTimer : MonoBehaviour
+{
+    public float tempTimer;
+    // Update is called once per frame
+    /*void Update()
+    {
+        tempTimer *= 100;
+        tempTimer = tempTimer % 1;
+        tempTimer /= 100;
+        Debug.Log(tempTimer.ToString());
+    }
+    */
+}
