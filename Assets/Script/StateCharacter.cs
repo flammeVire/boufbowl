f9bc9f4e525050f0abf93f7ed8843c0af939d6c2
Dartagnan17450 <julien.misery@gmail.com>
2025-12-04 17:50:42 +0100

    Maj ScriptableObject
    4 class et state rendu

--- Assets/Script/StateCharacter.cs
+++ Assets/Script/StateCharacter.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class StatCharacter : ScriptableObject {
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/StatCharacter", order = 1)]
+public class StatCharacter : ScriptableObject
+{
     //public
-    public float speed = 0f;
+    public float pv = 0f;
+    public float strength = 0f;
     public int pointMouvementMax = 0;
     public float rangeThrow = 0f;
-    public bool isStun = false; 
-    //priver
+    public bool isStun = false;
+    public bool isLunch = false;
+    public CharacterClass characterClass;
+    [System.Serializable]
+    public enum CharacterClass {
+        Captain,
+        Striker1,
+        Striker2,
+        Defender}
+    public CharacterClassArms characterClassArms;
+    [System.Serializable]
+    public enum CharacterClassArms {
+        Glove,
+        Shield}
 }
