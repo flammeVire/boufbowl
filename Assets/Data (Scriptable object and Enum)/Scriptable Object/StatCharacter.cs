c16447b1900b69c4bb2f9f4fcaeb8f606ea9e848
Ethan Deschamps Pro <146001193+PunkKin@users.noreply.github.com>
2025-12-04 21:57:09 +0100

    Redid the character stat script

--- Assets/Data (Scriptable object and Enum)/Scriptable Object/StatCharacter.cs
+++ Assets/Data (Scriptable object and Enum)/Scriptable Object/StatCharacter.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/StatCharacter", order = 1)]
 public class StatCharacter : ScriptableObject
 {
-    //public
+    //health of players
     public float pv = 0f;
+
+    // ???
     public float strength = 0f;
+
+    // Player Movement
     public int pointMouvementMax = 0;
+
+    // Range of the throws
     public float rangeThrow = 0f;
+
+    // Is the player in a stunned state
     public bool isStun = false;
-    public bool isLunch = false;
+
+    // Number of action point the player have left
+    public int actionPoint = 1;
+
+    // Number of movement point left
+    public int movementPoint = 1;
+
     public CharacterClass characterClass;
     [System.Serializable]
     public enum CharacterClass {
---
+++ Assets/Script/Use Action.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseAction : MonoBehaviour
+{
+
+    private IEnumerator coroutine;
+
+    
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(“left”))
+        {
+
+        }
+        else if (Input.GetKey(“right”))
+        {
+
+        }
+        else if ((Input.GetKey(“space”)))
+        {
+
+        }
+    }
+
+    private IEnumerator 
+
+    void RefreshActions(StatCharacter player)
+    {
+        player.actionPoint = 1;
+        player.movementPoint = 1;
+    }
+    */
+}
