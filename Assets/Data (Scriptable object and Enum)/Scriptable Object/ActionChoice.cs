5de7ffed68943063389297583743547534e4bf00
Ethan Deschamps Pro <146001193+PunkKin@users.noreply.github.com>
2025-12-04 23:47:58 +0100

    [+] Action Wheel

---
+++ Assets/Data (Scriptable object and Enum)/Scriptable Object/ActionChoice.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Action", order = 4)]
+
+public class ActionChoice : ScriptableObject
+{
+    // XXX
+
+}
+
